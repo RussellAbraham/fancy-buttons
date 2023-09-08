73512ec2c9222c06fab513af467163f86665b4d5
RussellAbraham <russell_a@live.com>
2023-09-07 18:15:11 -0700

    Add event handlers to setState of components

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -3,9 +3,15 @@ import { useState } from "react";
 function AngryButton(){
   
   const [anger, setAnger] = useState(0);
-
+  const handleClick = (amount)=> {
+    setAnger(anger < 1 ? anger + amount : 0);
+  };
   return (
-    <button style={{ backgoundColor:`rgba(255,0,0,${anger})` }} className="AngryButton">
+    <button 
+      style={{ backgroundColor:`rgba(255,0,0,${anger})` }} 
+      onClick={()=>handleClick(0.1)}
+      className="AngryButton"
+    >
       {anger < 1 && <span>Don't click me too much! </span>}
       {anger > 1 && <span>Rawr!</span>}
     </button>
